fba05c39ecb251c33c5419165436ff271800d4a4
Kazuki Asakura <arthur.1121lemsche@gmail.com>
2019-07-16 11:51:54 +0900

    スクレイピング完成

--- scraper.py
+++ scraper.py
@@ -1,11 +1,61 @@
 # カラオケランキング情報を取得し、その歌詞をtxtファイルで取得する
-
 __author__ = 'Kazuki Asakura'
 
 import urllib.request
 from bs4 import BeautifulSoup
+import csv
 import re
 import time
+
+# 定数
+KARAOKE_RANKINGS_DIR = './karaoke_rankings/'
+USEN_RANKINGS_DIR = './usen_rankings/'
+LYRICS_DIR = './lyrics/'
+RANKING_NUM = 20
+BEGIN_YEAR = 1980
+END_YEAR = 2018
+
+song_list = []
+
+# ランキングを取得し、CSVに書き出す
+def get_ranking(term):
+    global song_list
+    if term == 'karaoke':
+        url_term = 'karaoke'
+    elif term == 'usen':
+        url_term = 'usen_rank'
+    else:
+        raise Exception
+    # 開始年から終了年までループ
+    for year in range(BEGIN_YEAR, END_YEAR + 1):
+        print('Download Ranking Table: ' + term + '/' + str(year) + '年')
+        # 書き込むCSVを開く
+        f = open(KARAOKE_RANKINGS_DIR + str(year) + '.csv', 'w')
+        writer = csv.writer(f, lineterminator='\n')
+        # URLにアクセス
+        url = 'https://entamedata.web.fc2.com/music2/' + url_term + str(year) + '.html'
+        response = urllib.request.urlopen(url)
+        bs = BeautifulSoup(response.read(), 'lxml')
+        time.sleep(1)
+        # ランキングの要素を選択
+        table = bs.find('table', attrs={'cellpadding': '5'})
+        rows = table.findAll('tr')
+        # 各行のデータを取得しCSVに書き込む
+        for rank in range(1, RANKING_NUM + 1):
+            row = rows[rank]
+            cells = row.findAll('td', class_='sample3')
+            title = cells[0].find('a').string
+            artist = cells[1].find('a').string
+            writer.writerow([str(rank), title, artist])
+            # まだその曲の歌詞を取得していないなら取得
+            if (title + artist) not in song_list:
+                print('    Download Lyric: ' + title + '/' + artist)
+                song_list.append(title + artist)
+                try:
+                    get_lyric(title, artist)
+                except AttributeError:
+                    print('        Not Found.')
+        f.close()
 
 # 曲名とからJ-LyricのURLを得る
 def get_lyric_url(title, artist):
@@ -20,24 +70,25 @@ def get_lyric_url(title, artist):
     # URLが含まれる要素を選択
     element = bs.find('div', id='mnb').find('div', class_='bdy').find('p', class_='mid').find('a')
     # 属性値からURLを得る
-    url = element.get('href')
-    return url
+    lyric_url = element.get('href')
+    return lyric_url
 
 
-# 歌詞をtxtファイルに保存する
-def get_lyric(url):
+# 歌詞をtxtファイルに書き出す
+def get_lyric(title, artist):
     # URLにアクセス
+    url = get_lyric_url(title=title, artist=artist)
     response = urllib.request.urlopen(url)
     bs = BeautifulSoup(response.read(), 'lxml')
+    time.sleep(1)
     # 歌詞が含まれる要素を選択
     element = bs.find('div', id='mnb').find('div', class_='lbdy').find('p', id='Lyric')
     # 対象要素から不要なタグを削除
     lyric = str(element).replace('<p id="Lyric">', '').replace('</p>', '').replace('<br/>', '\n')
     # ファイルに出力
-    f = open('test.txt', 'w')
+    f = open(LYRICS_DIR + title + '_' + artist + '.txt', 'w')
     f.write(lyric)
     f.close()
 
-url = get_lyric_url(title='真夏の夜の夢', artist='松任谷由実')
-print(url)
-get_lyric(url)
+get_ranking('karaoke')
+get_ranking('usen')
